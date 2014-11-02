86a5832a8381e6d72930790644f1066af8fdb66b
Andrew Lilja <andrewlilja@gmail.com>
2014-11-02 15:07:48 -0600

    breweries work

--- ratebeer-data/ratebeer.py
+++ ratebeer-data/ratebeer.py
@@ -4,14 +4,34 @@ from pprint import pprint
 
 class RateBeer():
     def __init__(self):
-        self.search_url = "http://www.ratebeer.com/search.php"
+        self.BASE_URL = "http://www.ratebeer.com"
 
     def search(self, query):
         # this feels bad to me
         # but if it fits, i sits
-        payload = {"id": "myform2", "BeerName": query, "SortBy": 1}
-        r = requests.post(self.search_url, data = payload)
+        output = {"breweries":[],"beers":[]}
+
+        payload = {"BeerName": query}
+        r = requests.post(self.BASE_URL+"/findbeer.asp", data = payload)
         soup = BeautifulSoup(r.text)
+        s_results = soup.find_all('table',{'class':'results'})
+
+        # find the brewery information
+        s_breweries = s_results[0].find_all('tr')
+        for row in s_breweries:
+            location = row.find('td',{'align':'right'})
+            output['breweries'].append({
+                "name":row.a.contents,
+                "url":row.a.get('href'),
+                "location":location.contents[0].strip()
+            })
+        return output
+
+    def beer(self, name):
+        pass
+
+    def brewery(self, name):
+        pass
 
 rb = RateBeer()
-pprint(rb.search("summit extra pale ale"))
+pprint(rb.search("summit"))
