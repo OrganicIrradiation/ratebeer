8a8a253266965450da9e028e083e578651962b06
Andrew Lilja <andrewlilja@gmail.com>
2014-11-25 21:04:23 -0600

    setup.py description formatting

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 
 try:
-from setuptools import setup
+    from setuptools import setup
 except ImportError:
-from distutils.core import setup
+    from distutils.core import setup
 
 
 setup(
     name="ratebeer",
     version="1.3",
-    description="""RateBeer.com data scraper. Makes getting information from RateBeer.com as easy as:
-
-    >>> rb = RateBeer()
-    >>> rb.search("summit extra pale ale")
-    {'beers': [{'name': [u'Summit Extra Pale Ale'],
-        'num_ratings': 678,
-        'rating': 73  ,
-        'url': u'/beer/summit-extra-pale-ale/7344/'}],
-    'breweries': []}
-
+    description="""RateBeer.com data scraper. Makes getting information from RateBeer.com as easy as:<br />
+    <br />
+    >>> rb = RateBeer()<br />
+    >>> rb.search("summit extra pale ale")<br />
+    {'beers': [{'name': [u'Summit Extra Pale Ale'],<br />
+        'num_ratings': 678,<br />
+        'rating': 73  ,<br />
+        'url': u'/beer/summit-extra-pale-ale/7344/'}],<br />
+    'breweries': []}<br />
+    <br />
     See the full README at https://github.com/alilja/ratebeer
     """,
     keywords="ratebeer rate beer ratings",
