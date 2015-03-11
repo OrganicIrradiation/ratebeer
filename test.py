e0e33b57b6af9db6c5c4db01232747a2fff99ef7
Steven A. Cholewiak <scholewiak@gmail.com>
2015-03-11 21:47:21 +0100

    Added a number of new tests and reorganized them in alphabetical order (the way unittest runs them).

--- test.py
+++ test.py
@@ -6,33 +6,6 @@ from ratebeer import RateBeer
 
 
 class TestSearch(unittest.TestCase):
-    def test_search(self):
-        ''' Test out the search function with str and unicode searches '''
-        results = RateBeer().search("deschutes inversion")
-        self.assertListEqual(results['breweries'], [])
-        self.assertIsNotNone(results['beers'])
-        self.assertDictContainsSubset({
-            'url': '/beer/deschutes-inversion-ipa/55610/',
-            'name': u'Deschutes Inversion IPA',
-            'id': 55610
-        }, results['beers'][0])
-
-        # Test a beer that requires encoding
-        results = RateBeer().search("to øl jule mælk")
-        self.assertListEqual(results['breweries'], [])
-        self.assertIsNotNone(results['beers'])
-        self.assertDictContainsSubset({
-            'url': '/beer/to-ol-jule-maelk/235066/',
-            'name': u'To Øl Jule Mælk',
-            'id': 235066
-        }, results['beers'][0])
-
-    def test_beer_404(self):
-        ''' Checks to make sure that we appropriately raise a page not found '''
-        rb = RateBeer()
-        self.assertRaises(rb.PageNotFound, rb.beer, "/beer/sdfasdf")
-        self.assertIsNotNone(rb.beer("/beer/new-belgium-tour-de-fall/279122/"))
-
     def test_beer(self):
         ''' Make sure the results for a beer contain the expected data '''
         results = RateBeer().beer("/beer/new-belgium-tour-de-fall/279122/")
@@ -46,17 +19,12 @@ class TestSearch(unittest.TestCase):
             'ibu': 38
         }, results)
 
-        results = RateBeer().beer("/beer/deschutes-inversion-ipa/55610/")
-        self.assertIsNotNone(results)
-        self.assertDictContainsSubset({
-            'name': u'Deschutes Inversion IPA',
-            'brewery': u'Deschutes Brewery',
-            'brewery_url': u'/brewers/deschutes-brewery/233/',
-            'brewery_country': u'USA',
-            'style': u'India Pale Ale (IPA)',
-            'ibu': 80
-        }, results)
+    def test_beer_404(self):
+        ''' Checks to make sure that we appropriately raise a page not found '''
+        rb = RateBeer()
+        self.assertRaises(rb.PageNotFound, rb.beer, "/beer/sdfasdf")
 
+    def test_beer_country(self):
         results = RateBeer().beer("/beer/rochefort-trappistes-10/2360/")
         self.assertIsNotNone(results)
         self.assertDictContainsSubset({
@@ -68,6 +36,17 @@ class TestSearch(unittest.TestCase):
             'abv': 11.3
         }, results)
 
+    def test_beer_unicode(self):
+        results = RateBeer().beer("/beer/steoji-oktoberbjor/292390/")
+        self.assertIsNotNone(results)
+        self.assertDictContainsSubset({
+            'name': u'Steðji Októberbjór',
+            'brewery': u'Brugghús Steðja',
+            'brewery_url': u'/brewers/brugghus-steoja/15310/',
+            'brewery_country': u'Iceland',
+            'style': u'Spice/Herb/Vegetable'
+        }, results)
+
     def test_brewery(self):
         ''' Make sure the results for a brewery contain the expected data '''
         results = RateBeer().brewery("/brewers/deschutes-brewery/233/")
@@ -76,6 +55,21 @@ class TestSearch(unittest.TestCase):
             'name': u'Deschutes Brewery',
             'type': u'Microbrewery',
             'city': u'Bend',
+        }, results)
+
+    def test_brewery_404(self):
+        ''' Make sure the results for a brewery contain the expected data '''
+        rb = RateBeer()
+        self.assertRaises(rb.PageNotFound, rb.beer, "/brewers/qwerty/1234567890")
+
+    def test_brewery_unicode(self):
+        ''' Check unicode brewery URLs '''
+        results = RateBeer().brewery("/brewers/brauhaus-18•80/12750/")
+        self.assertIsNotNone(results)
+        self.assertDictContainsSubset({
+            'name': u'Brauhaus 18\x9580',
+            'type': u'Brew Pub',
+            'country': u'Germany',
         }, results)
 
     def test_contract_brewery(self):
@@ -94,6 +88,28 @@ class TestSearch(unittest.TestCase):
         ''' Check to make multi-page review searches work properly '''
         reviews = [r for r in RateBeer().reviews("/beer/triumph-rauchbier/37254/")]
         self.assertIsNotNone(reviews)
+
+    def test_search(self):
+        ''' Test out the search function with a str search '''
+        results = RateBeer().search("deschutes inversion")
+        self.assertListEqual(results['breweries'], [])
+        self.assertIsNotNone(results['beers'])
+        self.assertDictContainsSubset({
+            'url': '/beer/deschutes-inversion-ipa/55610/',
+            'name': u'Deschutes Inversion IPA',
+            'id': 55610
+        }, results['beers'][0])
+
+    def test_unicode_search(self):
+        ''' Test out the search function with a unicode search '''
+        results = RateBeer().search("to øl jule mælk")
+        self.assertListEqual(results['breweries'], [])
+        self.assertIsNotNone(results['beers'])
+        self.assertDictContainsSubset({
+            'url': '/beer/to-ol-jule-maelk/235066/',
+            'name': u'To Øl Jule Mælk',
+            'id': 235066
+        }, results['beers'][0])
 
 if __name__ == '__main__':
     unittest.main()
