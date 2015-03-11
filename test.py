efc3d6eed948121a8b9cfd3af69caccb0825b07e
Steven A. Cholewiak <scholewiak@gmail.com>
2015-03-11 20:29:13 +0100

    Added some comments to test.py and corrected an error with contract brewery beer results (since it has been recently changed to a generator).

--- test.py
+++ test.py
@@ -5,11 +5,13 @@ from ratebeer import RateBeer
 
 class TestSearch(unittest.TestCase):
     def test_beer_404(self):
+        ''' Checks to make sure that we appropriately raise a page not found '''
         rb = RateBeer()
         self.assertRaises(rb.PageNotFound, rb.beer, "/beer/sdfasdf")
         self.assertIsNotNone(rb.beer("/beer/new-belgium-tour-de-fall/279122/"))
 
     def test_beer(self):
+        ''' Make sure the results for a beer contain the expected data '''
         results = RateBeer().beer("/beer/new-belgium-tour-de-fall/279122/")
         self.assertIsNotNone(results)
         self.assertDictContainsSubset({
@@ -31,6 +33,7 @@ class TestSearch(unittest.TestCase):
         }, results)
 
     def test_brewery(self):
+        ''' Make sure the results for a brewery contain the expected data '''
         results = RateBeer().brewery("/brewers/deschutes-brewery/233/")
         self.assertIsNotNone(results)
         self.assertDictContainsSubset({
@@ -40,6 +43,8 @@ class TestSearch(unittest.TestCase):
         }, results)
 
     def test_contract_brewery(self):
+        ''' Beers brewed at contract brewers will have 'brewed_at' and
+            'brewed_at_url' fields, in addition to the normal fields '''
         results = RateBeer().brewery("/brewers/minhas-craft-brewery/1185/")
         self.assertIsNotNone(results)
         self.assertDictContainsSubset({
@@ -47,13 +52,15 @@ class TestSearch(unittest.TestCase):
             'brewed_at_url': '/brewers/minhas-craft-brewery/1185/',
             'brewery': u'Berghoff Brewery Inc.',
             'brewery_url': '/brewers/berghoff-brewery-inc/15529/'
-        }, results['beers'][0])
+        }, results['beers'].next())
 
     def test_reviews(self):
+        ''' Check to make multi-page review searches work properly '''
         reviews = [r for r in RateBeer().reviews("/beer/triumph-rauchbier/37254/")]
         self.assertIsNotNone(reviews)
 
     def test_search(self):
+        ''' Test out the search function '''
         results = RateBeer().search("deschutes inversion")
         self.assertListEqual(results['breweries'], [])
         self.assertIsNotNone(results['beers'])
