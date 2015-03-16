49f3810f9e38e5b5f08ecebf6e2827aa5cb76987
Andrew Lilja <andrewlilja@gmail.com>
2015-03-16 13:53:02 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except ImportError:
 
 setup(
     name="ratebeer",
-    version="1.4",
+    version="2.0",
     description="Python API for RateBeer.com",
     long_description=__doc__,
     keywords="ratebeer rate beer ratings",
