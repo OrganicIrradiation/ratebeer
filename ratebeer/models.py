f800f3c9410c417e08ce01e08fa989ab3c23b051
Parry Cadwallader <parrycadwallader@gmail.com>
2017-06-24 11:50:18 -0700

    Ignore ABVs of a beer in brewery list when it is -

--- ratebeer/models.py
+++ ratebeer/models.py
@@ -374,7 +374,7 @@ class Brewery(object):
             weighted_avg = row.findAll('td')[4].text.strip()
             style_rating = row.findAll('td')[5].text.strip()
             num_ratings = row.findAll('td')[6].text.strip()
-            if abv:
+            if abv and abv != '-':
                 beer.abv = float(abv)
             if weighted_avg:
                 beer.weighted_avg = float(weighted_avg)
